e65d3d5d496353a01178d485a95f2b8617c4ee70
Archiclouds <geraldomonog@gmail.com>
2026-03-18 19:00:31 -0300

    del
    Deletando

--- src/pages/Index.tsx
+++
@@ -1,7 +0,0 @@
-const Index = () => {
-  return (
-    <div className="min-h-[100svh] w-full bg-background antialiased overflow-hidden" style={{ touchAction: "none", cursor: "default" }} />
-  );
-};
-
-export default Index;
